aabb5f3f9a001f54387e74edc5d29be25e46df59
royce-git <54796100+royce-git@users.noreply.github.com>
2020-03-28 14:28:34 +0100

    new and edit routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,8 +11,9 @@ const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full'},
   { path: 'recipes', component: RecipesComponent, children: [
     { path: '', component: RecipeStartComponent },
+    { path: 'new', component: RecipeEditComponent },
     { path: ':id', component: RecipeDetailComponent },
-    { path: 'new', component: RecipeEditComponent }
+    { path: ':id/edit', component: RecipeEditComponent }
   ]},
   { path: 'shopping-list', component: ShoppingListComponent}
 ];
